46995a9dcd420e7ced320ecf7fed79197651a048
Sow Amadou <amadou@Sows-MBP.lan>
2024-11-10 12:25:30 +0100

    fix et ajout de l'animation de neji

--- ressource/js/main.js
+++ ressource/js/main.js
@@ -13,16 +13,19 @@ function fadeOutTitle(query) {
 
 function fadeInTitle(query) {
     const title = document.querySelector(query);
-    
-    title.classList.remove('hidden');
-    title.classList.remove('fadeOut');
-    title.classList.add('fadeIn');
+    if(title){
+        console.log(title)
+        title.classList.remove('hidden');
+        title.classList.remove('fadeOut');
+        title.classList.add('fadeIn');
+    }
 }
 
 function startAnimation(){
     const canvas = document.getElementById('canvas');
     canvas.style.display = 'block'; 
 
+    console.log(canvas)
     const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
@@ -40,7 +43,7 @@ function startAnimation(){
     sceneManager.addScene('ressource/model/scene2/scene.gltf', 'ressource/srt/introduction.srt', '',{ loop: false, toons:true, });
     sceneManager.addScene('ressource/model/introduction/scene.gltf', 'ressource/srt/example.srt', '',{ loop: true, skybox: true, toons:false, fog: {color: 0xec9f53, near: 18, far: 200 }, });
     sceneManager.addScene('ressource/model/scene1/scene.gltf', 'ressource/srt/example.srt', '',{ loop: true, skybox: true, toons:false, fog: {color: 0xec9f53, near: 18, far: 200 }, });
-    sceneManager.addScene('ressource/model/foret/scene.gltf', 'ressource/srt/example.srt', '',{ loop: true, skybox: false, toons:true, });
+    // sceneManager.addScene('ressource/model/foret/scene.gltf', 'ressource/srt/example.srt', '',{ loop: true, skybox: true, toons:false, });
 
     // Load the first scene
     sceneManager.loadScene(
@@ -67,9 +70,25 @@ function startAnimation(){
                         fadeInTitle('.NrTitle')
                     },3000)
                     break;
+                // case sceneManager.scenes.length :
+                //     fadeOutTitle(".canvas")
+                //     fadeInTitle(".resume")
+                //     break;
             }
 
-        })
+        });
+
+        sceneManager.eventEmitter.on("noSceneLeft",() => {
+            fadeOutTitle("#canvas");
+            fadeOutTitle("#Chapitre")
+
+            document.body.style.backgroundColor = "black";
+            fadeInTitle(".resume");
+            // console.log( document.querySelector("#Chapitre"));
+            // document.querySelector("#Chapitre").classList.add('hidden');
+            // console.log( document.querySelector("#Chapitre"));
+        });
+
         animate();
     });
 
@@ -83,5 +102,6 @@ function startAnimation(){
 // // Ajouter un écouteur d'événement pour le bouton "Start" et le rendre invisible une fois l'animation lancée
 // document.getElementById("startAnimation").addEventListener('click', function() {
 //     startAnimation();
-// });
+// // });
+
 startAnimation();
